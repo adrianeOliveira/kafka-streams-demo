22c1d66230e0da7ee48ed58b604404c3ddd5bee0
Adriane <rodrigues.adriane@outook.com>
2022-03-22 20:22:34 -0300

    Ãdicionado @Component no consumidor da aplicação

--- src/main/java/com/adriane/demo/kafkastreams/AppConsumer.java
+++ src/main/java/com/adriane/demo/kafkastreams/AppConsumer.java
@@ -3,8 +3,10 @@ package com.adriane.demo.kafkastreams;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 public class AppConsumer {
 
     @KafkaListener(topics = {KafkaStreamsConfig.STREAM_OUTPUT_TOPIC}, groupId = "demo.kafkastreams.out")
